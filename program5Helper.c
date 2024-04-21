e9c18dacf9272c8b120bd418e1aaa265fe97eb40
priyanka-bhutte <132783153+priyanka-bhutte@users.noreply.github.com>
2024-04-21 13:31:51 +0530

    Add files via upload

---
+++ program5Helper.c
@@ -0,0 +1,8 @@
+#include "program5Header.h"
+
+unsigned int Addition(unsigned int iValue1, unsigned int iValue2)
+{
+    unsigned int iResult = 0;
+    iResult = iValue1 + iValue2;
+    return iResult;
+}
